494895ce503bf8e08da14e25abe7ec82ce2b77b7
ranjith2007 <ranjithkumar0799@gmail.com>
2025-03-19 16:04:57 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source = "../terraform-aws-vpc"
+  #source = "../terraform-aws-vpc"
+  source = "git::https://github.com/ranjith2007/terraform-aws-vpc.git?ref=main"
   vpc_cidr = var.vpc_cidr
   project_name = var.project_name
   environment =  var.environment
